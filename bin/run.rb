ea174108aa785d3ed86469544ea875e681e69026
MarSchlaefer <mschlaefer00@gmail.com>
2018-09-12 13:07:17 -0400

    Syns and rhymes

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
-
+require 'pry'
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+
+
+require 'rest-client'
+require 'JSON'
+
+
+
+
 
 welcome
 character = get_character_from_user
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,36 +2,101 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_words_from_api(url)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get(url)
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def welcome
+  # welcomes and gives first instruction
+  puts "Welcome! Type a word you want to shorten."
 end
 
-def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+def get_text
+  # chomp the inputted word
+  gets.chomp
 end
 
-## BONUS
+def query(query_type, input)
+  # interpolate type and word into url
+  # call #get_words_from_api and inputs created url
+  # returns an array of words
+  url = "https://api.datamuse.com/words?rel_#{query_type}=#{input}"
+  get_words_from_api(url)
+end
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+def get_synonyms(input)
+  # talk to api and run appropriate queries
+  # return array of possible syns
+  query("syn", input)
+end
+
+def get_antonyms(input)
+  # talk to api and run appropriate queries
+  # return array of possible ants
+  query("ant", input)
+end
+
+def get_rhymes(input)
+  # talk to api and run appropriate queries
+  # return array of possible rhymes
+  query("rhy", input)
+end
+
+def filter_short_words(arr)
+  arr.select { |word| word.length < 6 }
+end
+
+def turn_text_to_synonyms
+  welcome
+  input = get_text.downcase
+  input_array = input.split(" ")
+  words_with_no_synonyms = []
+  output_array = input_array.map do |word|
+    if word.length >= 6
+      synonyms = get_synonyms(word) # array of syns
+      short_words = filter_short_words(synonyms) # array of short syns
+      if short_words != []
+        word_hash = short_words.sample # single short synonym, as a hash
+        new_word = word_hash["word"] # new word as string
+      else
+        words_with_no_synonyms << word
+      end
+    end
+  end
+  puts output_array.join(" ")
+  puts "The following words could not be shortened: #{words_with_no_synonyms}"
+  binding.pry
+end
+
+def turn_text_to_rhymes
+  welcome
+  input = get_text.downcase
+  input_array = input.split(" ")
+  words_with_no_rhymes = []
+  output_array = input_array.map do |word|
+    rhymes = get_rhymes(word) # array of rhymes
+    if rhymes != []
+      ####################################
+      # TO DO:
+      # REMOVE FUNCTION WORDS
+      # CHECK THAT SYLLABLES ARE THE SAME
+      # CHECK THAT PART OF SPEECH IS THE SAME
+      ####################################
+      word_hash = rhymes.sample # random rhyme
+      new_word = word_hash["word"] # new word as string
+    else
+      words_with_no_rhymes << word
+    end
+  end
+  puts output_array.join(" ")
+  puts "The following words had no rhymes: #{words_with_no_synonyms}"
+  binding.pry
+end
+
+turn_text_to_rhymes
+
+
+# binding.pry
